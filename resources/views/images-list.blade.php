9c60398bd4f4fb36b3968d3e55f5c4540187771d
Yaroslav Graboveckiy <yaroslav.graboveckiy@umbrella-web.com>
2017-11-21 20:43:51 +0300

    Finish first style of List of Images page and some global

--- resources/views/images-list.blade.php
+++ resources/views/images-list.blade.php
@@ -1,21 +1,83 @@
 @extends('base')
 
 @section('content')
+    <style>
+        body {
+            padding-top: 54px;
+        }
+
+        @media (min-width: 992px) {
+            body {
+                padding-top: 56px;
+            }
+        }
+
+        #buttons_div {
+            position: relative;
+
+        }
+
+        .container .name {
+            position: absolute;
+            text-align: left;
+            top: 50%;
+            -webkit-transform: translate(0, -50%);
+            -ms-transform: translate(0, -50%);
+            transform: translate(0, -50%);
+            height: 30px;
+            white-space: nowrap;
+            overflow: hidden;
+            background: white;
+            padding: 5px;
+            width: 60%;
+
+        }
+        .container .name::after {
+            content: ''; /* Выводим элемент */
+            position: absolute;
+            right: 0;
+            top: 0; /* Положение элемента */
+            width: 40px; /* Ширина градиента*/
+            height: 100%; /* Высота родителя */
+            /* Градиент */
+            background: -moz-linear-gradient(left, rgba(255, 255, 255, 0.2), #ffffff 100%);
+            background: -webkit-linear-gradient(left, rgba(255, 255, 255, 0.2), #ffffff 100%);
+            background: -o-linear-gradient(left, rgba(255, 255, 255, 0.2), #ffffff 100%);
+            background: -ms-linear-gradient(left, rgba(255, 255, 255, 0.2), #ffffff 100%);
+            background: linear-gradient(to right, rgba(255, 255, 255, 0.2), #ffffff 100%);
+        }
+        #buttons {
+            background-color: white;
+            opacity: 0.5;
+            width: 90%;
+            position: absolute;
+            text-align: right;
+            top: 89%;
+            padding-left: 5px;
+            -webkit-transform: translate(0, -50%);
+            -ms-transform: translate(0, -50%);
+            transform: translate(0, -50%);
+            height: 30px;
+        }
+
+    </style>
     <!-- Page Heading -->
     <h1 class="PageHead">Page Heading
         <small>Secondary Text</small>
     </h1>
-    <div class="row">
+    <div class="row text-center text-lg-left" id="buttons_div">
         @forelse($list as $image)
-            <div class="image_preview">
-                <img class="img" src="{{$image->image_url}}">
-                <p>{{$image->name}}</p>
-                <p>{{$image->album}}</p>
-                <p>{{$image->peoples}}</p>
-                <p>{{$image->place}}</p>
-                <p>{{$image->tags}}</p>
-                <a href="{{url('edit-image/'.$image->id)}}"><img class="icons" src="{{asset('images/edit.png')}}"></a>
-                <a href="{{url('#')}}"><img class="icons" src="{{asset('images/delete.png')}}"></a>
+            <div class="col-lg-3 col-md-4 col-xs-6 mb-4">
+                <img class="img-fluid img-thumbnail imglist" src="{{$image->image_url}}">
+                <div class="container" id="buttons">
+                    <div class="name">{{$image->name}}</div>
+                    <div class="control_buttons">
+                    <a href="{{url('edit-image/'.$image->id)}}"><img class="icons"
+                                                                     src="{{asset('images/edit.png')}}"></a>
+                    <a href="{{url('#')}}"><img class="icons" src="{{asset('images/delete.png')}}"></a>
+                    </div>
+
+                </div>
             </div>
         @empty
             <div class="message">Вы не загрузили ни одной фотографии</div>
