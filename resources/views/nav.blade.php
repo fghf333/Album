e7dfbe8d4eb4eafb3ae8f5c0c2d898437ed1cfa6
Yaroslav Graboveckiy <yaroslav.graboveckiy@umbrella-web.com>
2018-01-10 18:22:53 +0300

    add higliting for selected items in navbar

--- resources/views/nav.blade.php
+++ resources/views/nav.blade.php
@@ -3,23 +3,18 @@
 <!-- Navigation -->
 <nav class="navbar navbar-expand-lg navbar-dark bg-dark fixed-top">
     <div class="container">
-        <a class="navbar-brand" href="#">Семейный альбом</a>
+        <a class="navbar-brand" href="/">Семейный альбом</a>
         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarResponsive"
                 aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
         </button>
         <div class="collapse navbar-collapse" id="navbarResponsive">
             <ul class="navbar-nav ml-auto">
-                <li class="nav-item active">
-                    <a class="nav-link" href="/">На главную
-                        <span class="sr-only">(current)</span>
-                    </a>
-                </li>
-                <li class="nav-item">
+                <li class="nav-item {{ Request::is('albums') ? 'active' : '' }}">
                     <a class="nav-link" href="/albums">Альбомы</a>
                 </li>
                 <li class="nav-item">
-                    <a class="nav-link" href="/images-list">Список фотографий</a>
+                    <a class="nav-link {{ Request::is('images-list') ? 'active' : '' }}" href="/images-list">Список фотографий</a>
                 </li>
             </ul>
         </div>
