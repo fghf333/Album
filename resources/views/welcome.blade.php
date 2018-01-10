b32e4be4bfe7628e6a4a6d6fc651bf344f6d66d4
Yaroslav Graboveckiy <yaroslav.graboveckiy@umbrella-web.com>
2018-01-10 19:41:28 +0300

    Improve home page with last photos list

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,6 +1,21 @@
 @extends('base')
 @section('content')
     <!-- Page Heading -->
-    <h1 style="padding-top: 10vh; text-align: center;">Семейный альбом
-    </h1>
+    <h1 style="padding-top: 10vh; text-align: center;">Семейный альбом</h1>
+    <div class="row text-center text-lg-left" id="buttons_div">
+        @forelse($images as $image)
+            <div class="col-lg-3 col-md-4 col-xs-6 mb-4">
+                <a data-fancybox="image" href="{{$image->image_url}}"><img class="img-thumbnail"
+                                                                           src="{{$image->image_url}}"></a>
+                <div class="container" id="buttons">
+                    <div class="name">{{$image->name}}</div>
+                    <div class="control_buttons">
+                    </div>
+
+                </div>
+            </div>
+        @empty
+            <div class="message">Вы не загрузили ни одной фотографии</div>
+        @endforelse
+    </div>
 @endsection
