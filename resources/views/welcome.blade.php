69f3d5817a673ba19b9394f5da27c0417bfc59ea
Yaroslav Graboveckiy <yaroslav.graboveckiy@umbrella-web.com>
2018-01-17 22:27:08 +0300

    Fix styles and template for list of images

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,11 +4,14 @@
     <h1 class="home-message">Семейный альбом</h1>
     <div class="row text-center text-lg-left" id="buttons_div">
         @forelse($images as $image)
-            <div class="col-lg-3 col-md-4 col-xs-6 mb-4">
-                <a data-fancybox="image" href="{{$image->image_url}}"><img class="img-thumbnail"
-                                                                           src="{{$image->image_url}}"></a>
-                <div class="container buttons" id="buttons">
-                    <div class="name">{{$image->name}}</div>
+            <div class="col-lg-3 col-md-4 col-6 md-4">
+                <div class="img-container">
+                    <a data-fancybox="image" href="{{$image->image_url}}">
+                        <img class="rounded img-thumb" src="{{$image->image_url}}">
+                    </a>
+                    <div class="buttons">
+                        <div class="name">{{$image->name}}</div>
+                    </div>
                 </div>
             </div>
         @empty
