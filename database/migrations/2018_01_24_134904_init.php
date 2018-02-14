592e05f9301e7d168383d94297998333cf811a55
Yaroslav Graboveckiy <yaroslav.graboveckiy@umbrella-web.com>
2018-02-14 23:19:55 +0300

    update migration

--- database/migrations/2018_01_24_134904_init.php
+++ database/migrations/2018_01_24_134904_init.php
@@ -38,7 +38,7 @@ class Init extends Migration
             $table->dateTime('updated_at');
             $table->date('createdAt');
             $table->text('tags')->nullable();
-            $table->unique('image_id', 'image_url');
+            $table->unique('image_id');
         });
 
         //Create 'tags' table
@@ -50,6 +50,30 @@ class Init extends Migration
             $table->dateTime('updated_at');
             $table->unique('name');
         });
+
+        //Create 'users' table
+        Schema::create('users', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('username', 255);
+            $table->string('email', 255);
+            $table->string('password', 255);
+            $table->string('api_key', 255);
+            $table->string('api_secret', 255);
+            $table->string('cloud_name', 255);
+            $table->dateTime('updated_at');
+            $table->dateTime('created_at');
+            $table->string('remember_token', 255)->nullable();
+            $table->unique('email');
+        });
+
+        //Create 'password_resets' table
+        Schema::create('password_resets', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('email', 255);
+            $table->string('token', 255);
+            $table->dateTime('created_at');
+            $table->unique('email');
+        });
     }
 
     public function down()
@@ -57,6 +81,7 @@ class Init extends Migration
         Schema::drop('albums');
         Schema::drop('images');
         Schema::drop('tags');
-        Schema::drop('migrations');
+        Schema::drop('users');
+        Schema::drop('password_resets');
     }
 }
