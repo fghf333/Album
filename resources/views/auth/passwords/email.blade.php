6a103af28dd9ea49876e0cce44df7943f9725218
Yaroslav Graboveckiy <yaroslav.graboveckiy@umbrella-web.com>
2018-02-15 00:54:36 +0300

    Implement reset password functionality

--- resources/views/auth/passwords/email.blade.php
+++ resources/views/auth/passwords/email.blade.php
@@ -1,47 +1,34 @@
-@extends('layouts.app')
+@extends('base')
 
 @section('content')
-<div class="container">
+    <h1 class="PageHead">Сброс пароля</h1>
     <div class="row">
-        <div class="col-md-8 col-md-offset-2">
-            <div class="panel panel-default">
-                <div class="panel-heading">Reset Password</div>
-
-                <div class="panel-body">
-                    @if (session('status'))
-                        <div class="alert alert-success">
-                            {{ session('status') }}
-                        </div>
-                    @endif
-
-                    <form class="form-horizontal" method="POST" action="{{ route('password.email') }}">
-                        {{ csrf_field() }}
-
-                        <div class="form-group{{ $errors->has('email') ? ' has-error' : '' }}">
-                            <label for="email" class="col-md-4 control-label">E-Mail Address</label>
-
-                            <div class="col-md-6">
-                                <input id="email" type="email" class="form-control" name="email" value="{{ old('email') }}" required>
-
-                                @if ($errors->has('email'))
-                                    <span class="help-block">
+        <div class="col-md-6 align-items-center offset-md-3">
+            <div class="panel-body">
+                @if (session('status'))
+                    <div class="alert alert-success">
+                        {{ session('status') }}
+                    </div>
+                @endif
+                <form method="POST" action="{{ route('password.email') }}">
+                    {{ csrf_field() }}
+                    <div class="form-group">
+                        <label for="email">Имейл адрес:</label>
+                        <input id="email" type="email" class="form-control" name="email" value="{{ old('email') }}"
+                               required>
+                        @if ($errors->has('email'))
+                            <span class="help-block">
                                         <strong>{{ $errors->first('email') }}</strong>
                                     </span>
-                                @endif
-                            </div>
-                        </div>
-
-                        <div class="form-group">
-                            <div class="col-md-6 col-md-offset-4">
-                                <button type="submit" class="btn btn-primary">
-                                    Send Password Reset Link
-                                </button>
-                            </div>
-                        </div>
-                    </form>
-                </div>
+                        @endif
+                    </div>
+                    <div class="form-group flex-center">
+                        <button type="submit" class="btn btn-primary">
+                            Отправить ссылку для восстановления
+                        </button>
+                    </div>
+                </form>
             </div>
         </div>
     </div>
-</div>
 @endsection
