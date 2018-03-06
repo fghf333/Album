ce525dbfb2c939f0667960cc5364f48c4401f2d6
Yaroslav Graboveckiy <yaroslav.graboveckiy@umbrella-web.com>
2018-03-06 22:51:45 +0300

    Prepare simple profile page

--- app/Http/Controllers/ProfileController.php
+++ app/Http/Controllers/ProfileController.php
@@ -9,9 +9,20 @@
 namespace App\Http\Controllers;
 
 
+use Illuminate\Support\Facades\Auth;
+
 class ProfileController extends Controller
 {
-   public function getProfile($UserID){
-       return view('profile');
+   public function getProfile(){
+       $user = Auth::user()['original'];
+       
+       $oldDate = date_create($user['created_at']);
+       $newDate = date_create(date('Y-m-d H:i:s'));
+       $dateDiff = date_diff($oldDate, $newDate)->format('%y Лет %m Месяцев %d Дней %h Часов %i Минут %s Секунд');
+       
+       $data['username'] = $user['username'];
+       $data['email'] = $user['email'];
+       $data['since'] = $dateDiff;
+       return view('profile', ['data' => $data]);
    }
 }
