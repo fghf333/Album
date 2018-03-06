070be160f7871cbe3828c2f7e22fad55187dd836
Yaroslav Graboveckiy <yaroslav.graboveckiy@umbrella-web.com>
2018-03-06 22:51:45 +0300

    Prepare simple profile page

--- resources/views/profile.blade.php
+++ resources/views/profile.blade.php
@@ -3,15 +3,15 @@
     <!-- Page Heading -->
     <h1 class="home-message">Hello World!</h1>
 
-    <div class="dropdown">
-        <button class="btn btn-secondary dropdown-toggle" data-toggle="dropdown" type="button" id="dropdownMenu2" aria-haspopup="true" aria-expanded="false">
-            Dropdown
-        </button>
-        <div class="dropdown-menu" aria-labelledby="dropdownMenu2">
-            <button class="dropdown-item" type="button">Action</button>
-            <button class="dropdown-item" type="button">Another action</button>
-            <button class="dropdown-item" type="button">Something else here</button>
-        </div>
-    </div>
+    <table class="table">
+        <tbody>
+@foreach($data as $key => $value)
+    <tr>
+        <td>{{$key}}</td>
+        <td>{{$value}}</td>
+    </tr>
+    @endforeach
+        </tbody>
+    </table>
 
 @endsection
