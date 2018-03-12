abcc230b9dfc7fee00f1a480dda1e168fffd9b07
Yaroslav Graboveckiy <yaroslav.graboveckiy@umbrella-web.com>
2018-03-12 14:24:11 +0300

    remove 'since' field

--- app/Http/Controllers/ProfileController.php
+++ app/Http/Controllers/ProfileController.php
@@ -9,21 +9,16 @@
 namespace App\Http\Controllers;
 
 
-use Carbon\Carbon;
 use Illuminate\Support\Facades\Auth;
 
 class ProfileController extends Controller
 {
    public function getProfile(){
+
        $user = Auth::user()['original'];
-       
-       $oldDate = new Carbon($user['created_at']);
-       $newDate = Carbon::now();
-       $dateDiff = $oldDate->diffForHumans($newDate,false,false, 6);
        
        $data['username'] = $user['username'];
        $data['email'] = $user['email'];
-       $data['since'] = $dateDiff;
        return view('profile', ['data' => $data]);
    }
 }
