27772a55e4d4ce5a9eed92aa65b8a4dc4bdf350b
Yaroslav Graboveckiy <yaroslav.graboveckiy@umbrella-web.com>
2018-06-25 18:07:20 +0300

    Add Spamer filter

---
+++ resources/views/errors/599.blade.php
@@ -0,0 +1,10 @@
+@extends('base')
+
+@section('content')
+
+    <!-- Page Heading -->
+    <h1 class="PageHead">СТОП!</h1>
+    <h2 class="PageHead">Ваш имейл находится в спам базе. Если это не так - свяжитесь с <a
+                href="mailto:{{env('APP_EMAIL')}}">администратором</a>.</h2>
+
+@endsection
