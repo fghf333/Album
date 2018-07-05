73734d4559762648deba07b7d481d471c1aa47b3
Yaroslav Graboveckiy <yaroslav.graboveckiy@umbrellait.com>
2018-07-05 20:14:56 +0300

    remove mocked user

--- database/seeds/InitSeeder.php
+++ database/seeds/InitSeeder.php
@@ -17,28 +17,13 @@ class InitSeeder extends Seeder
             'id' => 1,
             'name' => 'Неотсортированное',
             'creator' => 1,
-            'preview_img_id' => NULL,
+            'preview_img_id' => null,
             'shared' => 1,
-            'description' => NULL,
-            'preview_img' => NULL,
+            'description' => null,
+            'preview_img' => null,
             'photo_num' => 0,
             'updated_at' => date("Y-m-d H:i:s"),
             'created_at' => date("Y-m-d H:i:s"),
-        ]);
-        DB::table('users')->insert([
-            'id' => 1,
-            'username' => 'Mock',
-            'first_name' => 'Mock',
-            'last_name' => 'Mackovsky',
-            'family_id' => '0',
-            'email' => 'example@domain.com',
-            'password' => '1',
-            'api_key' => '1',
-            'api_secret' => '1',
-            'cloud_name' => 'placeholder',
-            'updated_at' => '2018-02-22 12:18:14',
-            'created_at' => '2018-02-22 12:18:15',
-            'family_admin' => 0,
         ]);
     }
 }
